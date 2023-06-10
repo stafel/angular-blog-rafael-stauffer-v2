6601ebb503030b32ba9e69dd2593aa8ed48ea111
Roger Rudin <roger.rudin@mobi.ch>
2023-06-10 16:48:41 +0200

    feat: contract validation

--- src/app/core/blog-data.service.ts
+++ src/app/core/blog-data.service.ts
@@ -1,7 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment.development';
-import { Blog } from './home/home.component';
+
+import { z } from 'zod';
+import { Observable, map } from 'rxjs';
+
+const BlogSchema = z.object({
+  id: z.number(),
+  title: z.string(),
+  contentPreview: z.string(),
+  author: z.string(),
+  likes: z.number(),
+  comments: z.number(),
+  likedByMe: z.boolean(),
+  createdByMe: z.boolean(),
+});
+
+const BlogArraySchema = z.array(BlogSchema);
+
+export type Blog = z.infer<typeof BlogSchema>;
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +26,9 @@ import { Blog } from './home/home.component';
 export class BlogDataService {
   constructor(private httpClient: HttpClient) {}
 
-  getBlogPosts() {
-    return this.httpClient.get<Blog[]>(`${environment.serviceUrl}/entries`);
+  getBlogPosts(): Observable<Blog[]> {
+    return this.httpClient
+      .get<Blog[]>(`${environment.serviceUrl}/entries`)
+      .pipe(map((blogs) => BlogArraySchema.parse(blogs)));
   }
 }
